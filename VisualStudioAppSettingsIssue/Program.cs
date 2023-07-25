ab0697229d220f13cce80005460aa449aee94c98
Ken Brannigan <ken@fewwillhunt.com>
2023-07-25 16:57:30 -0400

    Add project files.

---
+++ VisualStudioAppSettingsIssue/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace VisualStudioAppSettingsIssue;
+
+public class Program
+{
+    public static void Main()
+    {
+        IHostBuilder hostBuilder = Host.CreateDefaultBuilder();
+
+        IHost host = hostBuilder.Build();
+
+        // Get a value called testValue from the appsettings.json file
+        string? testValue = host.Services
+            .GetRequiredService<IConfiguration>()
+            .GetValue<string>("testValue");
+
+        Console.WriteLine($"testValue: {testValue}");
+        Console.ReadLine();
+    }
+}
